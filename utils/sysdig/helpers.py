fadc0e56073e49ddf820486441a47e21d6daa478
S3B4SZ17 <sebastian.zumbado@sysdig.com>
2025-10-03 10:37:45 -0600

    fix: Updating the tools permissions and updated docs to reflect minimum permissions
    Signed-off-by: S3B4SZ17 <sebastian.zumbado@sysdig.com>

--- utils/sysdig/helpers.py
+++ utils/sysdig/helpers.py
@@ -5,6 +5,6 @@ Helper functions for working with Sysdig API clients.
 # Sysdig permissions needed for the different set of tools
 TOOL_PERMISSIONS = {
     "cli-scanner": ["secure.vm.cli-scanner.exec"],
-    "threat-detection": ["custom-events.read"],
-    "sysql": ["sage.exec", "sage.manage.exec"],
+    "threat-detection": ["policy-events.read"],
+    "sysql": ["sage.exec", "risks.read"],
 }
