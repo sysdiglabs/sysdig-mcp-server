3c4a0fbddbce95d2db221604026fdd8f5d663fc6
Fede Barcelona <fede_rico_94@hotmail.com>
2026-01-20 15:32:06 +0100

    fix(build): support non-static build on Darwin

--- flake.nix
+++ flake.nix
@@ -11,7 +11,11 @@
     }:
     let
       overlays.default = final: prev: {
-        sysdig-mcp-server = prev.pkgsStatic.callPackage ./package.nix { };
+        sysdig-mcp-server =
+          if prev.stdenv.isLinux then
+            prev.pkgsStatic.callPackage ./package.nix { }
+          else
+            prev.callPackage ./package.nix { };
       };
       flake = flake-utils.lib.eachDefaultSystem (
         system:
