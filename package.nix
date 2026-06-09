e1ba128300b9b2eb3d29301e86b2e5e499d53645
Fede Barcelona <fede_rico_94@hotmail.com>
2026-06-09 11:52:35 +0200

    chore: update to 2.0.1

--- package.nix
+++ package.nix
@@ -1,7 +1,7 @@
 { buildGo126Module, versionCheckHook }:
 buildGo126Module (finalAttrs: {
   pname = "sysdig-mcp-server";
-  version = "2.0.0";
+  version = "2.0.1";
   src = ./.;
   # This hash is automatically re-calculated with `just rehash-package-nix`. This is automatically called as well by `just update`.
   vendorHash = "sha256-diD5m9+O7Qn7ln7F/R22Iaz2wNf7yth7ef4JEafrR9o=";
